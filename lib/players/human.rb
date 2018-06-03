b970738786c2107dffab8b53ea8003abf2428dfb
Matt Farmer <mrfarmer777@gmail.com>
2018-06-03 03:26:12 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,11 @@
-module Players
+module Players  #a module that holds the instance methods for the other types of players
   class Human < Player
     def move(board)
-      
+      puts "Select a position for your move: "
+      move=gets.strip
+      board[move.to_i-1]=self.token
+    end
+
 
 
 
